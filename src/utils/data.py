423b4c40a0b5f96bdb2d5235ac55921050c5d6af
Yulian Gilyazev <gilyazev.yulian@wb.ru>
2025-01-12 21:25:53 +0300

    update data scheme

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from typing import List
 import json
 from dataclasses import dataclass
@@ -11,7 +10,10 @@ class Dialogue:
     second_messages: List[str]
     emotion: Emotion = None
     scenario: str = None
-    alt_scenario: str = None
+    interlocutor_scenario: str = None
+    alt_last_message: str = None
+    empathy_label: int = None
+    id: int = None
 
     def format_dialogue(self) -> str:
         messages = [f"A: {first}\nB: {second}" for first, second in zip(self.first_messages, self.second_messages)]
@@ -19,35 +21,36 @@ class Dialogue:
 
 
 class SyntheticEmotionDataset:
-    def __init__(self, dialogues_paths: List[str], empathy_labels: List[str], scenario_path: str):
+    def __init__(self, dialogues_path: str, scenarios_path: str):
         self.dialogues = []
-        self.empathy_labels = []
 
-        with open(scenario_path, "r") as f:
+        with open(scenarios_path, "r") as f:
             data = json.load(f)["scenarios"]
             self.scenarios = {}
             for item in data:
                 self.scenarios[item["id"]] = item
                 
-        for dialogue_path, empathy_label in zip(dialogues_paths, empathy_labels):
-            with open(dialogue_path, "r") as f:
-                data = json.load(f)["dialogues"]
-            for dialogue in data:
-                scenario_id = dialogue["scenario_id"]
-                dialogue = Dialogue(
-                    first_messages=[item["content"] for item in dialogue["dialogue"] if item["role"] == "first"],
-                    second_messages=[item["content"] for item in dialogue["dialogue"] if item["role"] == "second"],
-                    emotion=Emotion.from_str(self.scenarios[scenario_id]["emotion"]),
-                    scenario=self.scenarios[scenario_id]["scenario"]["main"],
-                    alt_scenario=self.scenarios[scenario_id]["scenario"]["interlocutor"]
-                )
-                self.dialogues.append(dialogue)
-                self.empathy_labels.append(empathy_label)
+        with open(dialogues_path, "r") as f:
+            data = json.load(f)["dialogues"]
+
+        for data_item in data:
+            scenario_id = data_item["scenario_id"]
+            dialogue = Dialogue(
+                first_messages=[item["content"] for item in data_item["dialogue"] if item["role"] == "first"],
+                second_messages=[item["content"] for item in data_item["dialogue"] if item["role"] == "second"],
+                emotion=Emotion.from_str(self.scenarios[scenario_id]["emotion"]),
+                scenario=self.scenarios[scenario_id]["scenario"]["main"],
+                interlocutor_scenario=self.scenarios[scenario_id]["scenario"]["interlocutor"],
+                alt_last_message=data_item["alt_last_utterance"],
+                empathy_label=data_item["empathy_label"],
+                id=data_item["id"]
+            )
+            self.dialogues.append(dialogue)
 
     def __getitem__(self, idx):
         if idx >= len(self.dialogues):
             raise IndexError
-        return self.dialogues[idx], self.empathy_labels[idx]
+        return self.dialogues[idx]
 
     def __len__(self):
         return len(self.dialogues)
