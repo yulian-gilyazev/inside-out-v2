cccf158f96cce734eaba3b52d7c78c0befce6e55
gilyazev.yulian <gilyazev.yulian@wb.ru>
2025-03-09 12:12:04 +0000

    fix factory

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -60,19 +60,27 @@ class EchoAgent(Agent):
         return context
 
 class AgentFactory(ABC):
-    @staticmethod
-    def get_agent(agent_config_dct: dict, kwargs: dict = None) -> Agent:
-        classes: dict[Hashable, (Callable[..., object], Callable[..., object])] = {
-            "IO": (IOAgent, IOAgentConfig),
-            "Echo": (EchoAgent, AgentConfig),
-        }
-        classes_ = classes.get(agent_config_dct["agent_type"], None)
-        if classes_ is None:
+    __classes: dict[Hashable, (Callable[..., object], Callable[..., object])] = {
+        "IO": (IOAgent, IOAgentConfig),
+        "Echo": (EchoAgent, AgentConfig),
+    }
+    __use_llm: List[str] = ["IO"]
+
+    @classmethod
+    def add_agent(cls, agent_name, agent_cls, config_cls, use_llm: bool = False) -> None:
+        cls.__classes[agent_name] = (agent_cls, config_cls)
+        if use_llm:
+            cls.__use_llm.append(agent_name)
+        
+    @classmethod
+    def get_agent(cls, agent_config_dct: dict, kwargs: dict = None) -> Agent:
+        agent_classes = cls.__classes.get(agent_config_dct["agent_type"], None)
+        if agent_classes is None:
             raise ValueError(f"Agent type {agent_config_dct['agent_type']} not supported")
 
-        agent_class, agent_config_class = classes_
+        agent_class, agent_config_class = agent_classes
         agent_kwargs = {}
-        if agent_config_dct["agent_type"] == "IO":
+        if agent_config_dct["agent_type"] in cls.__use_llm:
             agent_kwargs["llm_client"] = kwargs.get("llm_client", None)
         agent = agent_class(agent_config_class(**agent_config_dct), **agent_kwargs)
         return agent
